591dac6d6acb6b3f21c90c7e3c3fd0a922433c83
TracyAntonia <aantonjuguna@gmail.com>
2022-11-28 08:14:03 +0300

    Completed assignment

---
+++ detector.js
@@ -0,0 +1,17 @@
+let speedLimit = 70
+function inputSpeed(){
+    if (speed <= 70){
+        return 'Ok'
+    }else if (speed = speedLimit + 5){
+        return 'Point 1'
+    } 
+}
+        
+function driverPoints(){
+    if (points = 12){
+    return 'Okay'
+    }else if (points >12){
+        return 'License suspended'
+    }
+
+}
